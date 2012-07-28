b1c6ed72938f9ac515a64864cfbb2d0f9f5c3fdc
Mike Baum <mike.baum@rogers.com>
2012-07-28 08:22:00 -0400

    refactor parameter set --broken--

--- src/org/mike/datatypes/Person.scala
+++ src/org/mike/datatypes/Person.scala
@@ -3,7 +3,7 @@ package org.mike.datatypes
 import org.mike.parameter.ParameterSet
 
 
-protected class Person extends ParameterSet {
+abstract protected class Person extends ParameterSet {
 	sealed trait Val[T] extends Parameter[T]
 	type PSET = Person
 }
@@ -12,4 +12,6 @@ object Person extends Person {
   case object FIRST_NAME extends Val[String]
   case object LAST_NAME extends Val[String]
   case object AGE extends Val[Int]
+  
+  override def buildVals = Set(FIRST_NAME, LAST_NAME, AGE)
 }
--- src/org/mike/parameter/ParameterSet.scala
+++ src/org/mike/parameter/ParameterSet.scala
@@ -4,13 +4,13 @@ trait ParameterSet {
   type Val[T] <: Parameter[T]
   type PSET <: ParameterSet
 
-  protected trait Parameter[T] {
-    self: Val[T] => _values += this
-    println( "added: " + this + " to " + ParameterSet.this )
-  }
+  protected trait Parameter[T]
 
-  private var _values = Set.empty[Val[_]]
+  lazy protected val _values = buildVals
   def values = _values
+  
+  // override and create a set containing all the defined Vals
+  protected def buildVals : Set[Val[_]]
   
   def apply() : ParameterList[PSET] = ParameterList[PSET]
 }
